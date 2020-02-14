9443db722e4f2b16fd908146f386814859488afa
bit01class <bit01class@gmail.com>
2020-02-14 16:41:33 +0900

    Spring JDBC - JdbcTemplate CRUD

--- sts01/src/com/bit/model/Emp02Dao2.java
+++ sts01/src/com/bit/model/Emp02Dao2.java
@@ -28,5 +28,55 @@ public class Emp02Dao2 extends JdbcDaoSupport {
 			
 		});
 	}
+	
+	public Emp02Vo selectOne(int sabun) {
+		String sql="select * from emp02 where sabun=?";
+		return this.getJdbcTemplate()
+				.queryForObject(sql, new RowMapper<Emp02Vo>() {
+
+					@Override
+					public Emp02Vo mapRow(ResultSet rs, int rowNum) throws SQLException {
+						return new Emp02Vo(
+								rs.getInt("sabun"),
+								rs.getString("name"),
+								rs.getDate("nalja"),
+								rs.getInt("pay")
+								);
+					}
+					
+				},new Object[] {sabun});
+	}
+	
+	public void insertOne(int sabun,String name,int pay) {
+		String sql="insert into emp02 values (?,?,sysdate,?)";
+		this.getJdbcTemplate().update(sql,new Object[] {sabun,name,pay});
+	}
+	
+	public int updateOne(int sabun,String name,int pay) {
+		String sql="update emp02 set name=?,pay=? where sabun=?";
+		return this.getJdbcTemplate().update(sql,new Object[] {name,pay,sabun});
+	}
+	
+	public int deleteOne(int sabun) {
+		String sql="delete from emp02 where sabun=?";
+		return this.getJdbcTemplate().update(sql,new Object[] {sabun});
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
